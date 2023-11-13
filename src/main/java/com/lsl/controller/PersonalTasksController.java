14fb7912cb671a8ee3a000a2375a8a8794e3af2a
Gaichifanla <luoyu_1223@163.com>
2023-11-13 22:11:54 +0800

    更新

--- src/main/java/com/lsl/controller/PersonalTasksController.java
+++ src/main/java/com/lsl/controller/PersonalTasksController.java
@@ -9,21 +9,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 @RestController
-@RequestMapping()
+@RequestMapping("/personal_list")
 @Slf4j
 public class PersonalTasksController {
     @Resource
     private PersonalTasksService personalTasksService;
 
-    @GetMapping()
+    @GetMapping("/getAllTasksByUserId")
     public Result getAllTasksByUserId(int userId){
         log.info("查询id为"+userId+"的任务列表");
         return Result.success(personalTasksService.getAllTasksByUserId(userId));
     }
-    @GetMapping()
+
+    @GetMapping("/insertPersonalTask")
     public Result insertPersonalTask(PersonalTasks personalTasks){
+        log.info("调用insertPersonalTask函数");
+        //log.info("调用函数");
         personalTasksService.insertPersonalTask(personalTasks);
         return Result.success(personalTasks);
     }
